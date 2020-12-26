bbd2e2c4e08a6da27b118f85f00c7c907e5751e6
Russkazkin <ruslan@skazkin.su>
2020-12-26 16:25:22 +0300

    Add App component code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,56 @@
+import {Component} from 'react';
+import {v4} from 'uuid';
 import './App.sass';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Color Picker</h1>
-    </div>
-  );
+export class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      colors: [],
+    }
+  }
+
+  addColor = (title, color) => {
+    const colors = [
+      ...this.state.colors,
+      {
+        id: v4(),
+        title,
+        color,
+        rating: 0,
+      }
+    ];
+  }
+
+  rateColor = (id, rating) => {
+    const colors = this.state.colors.map(color =>
+      (color.id !== id) ?
+        color :
+        {
+          ...color,
+          rating
+        }
+    )
+    this.setState({colors});
+  }
+
+  removeColor(id) {
+    const colors = this.state.colors.filter(
+      color => color.id !== id
+    )
+    this.setState({colors});
+  }
+
+  render = () => {
+    const { addColor, rateColor, removeColor } = this;
+    const { colors } = this.state;
+    return (
+      <div className="app">
+        <AddColorForm onNewColor={addColor} />
+        <ColorList colors={colors} onRate={rateColor} onRemove={removeColor} />
+      </div>
+    );
+  }
 }
 
-export default App;
+
