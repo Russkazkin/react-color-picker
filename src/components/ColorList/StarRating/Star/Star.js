9f13878b50c7baeceecf7ca91b7d013d63ef6096
Russkazkin <ruslan@skazkin.su>
2020-12-27 08:38:36 +0300

    Add Star sass module

--- src/components/ColorList/StarRating/Star/Star.js
+++ src/components/ColorList/StarRating/Star/Star.js
@@ -1,5 +1,7 @@
+import styles from './Star.module.sass';
+
 const Star = ({selected = false, onClick = f => f}) => (
-  <div className={(selected) ? "star selected" : "star"}
+  <div className={(selected) ? styles.Star.selected : styles.Star}
        onClick={onClick}>
   </div>
 );
