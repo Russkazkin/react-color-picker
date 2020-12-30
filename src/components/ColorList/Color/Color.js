e1d9985614e7514ec953f8fee1d418589df3fac9
Russkazkin <ruslan@skazkin.su>
2020-12-30 17:11:10 +0300

    Add color component close button tailwind classes

--- src/components/ColorList/Color/Color.js
+++ src/components/ColorList/Color/Color.js
@@ -5,7 +5,7 @@ const Color = ({ title, color, rating = 0, onRemove = f => f, onRate = f => f })
     <div className="color h-60 relative"
          style={{ backgroundColor: color }}>
       <h1 className="absolute w-full text-center font-bold text-white text-2xl top-1/3">{title}</h1>
-      <button onClick={onRemove}>X</button>
+      <button className="right-2 top-1 p-1 absolute text-white text-2xl" onClick={onRemove}>x</button>
     </div>
     <div>
       <StarRating starsSelected={rating} onRate={onRate} />
