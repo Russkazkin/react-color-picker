dbc2738f5c6f08920c5c8157730a219a88cc2d07
Russkazkin <ruslan@skazkin.su>
2020-12-31 06:15:55 +0300

    Fix StarRating component

--- src/components/ColorList/StarRating/StarRating.js
+++ src/components/ColorList/StarRating/StarRating.js
@@ -3,7 +3,7 @@ import Star from "./Star/Star";
 const StarRating = ({starsSelected = 0, totalStars = 5, onRate = f => f}) => (
   <div className="star-rating">
     {[...Array(totalStars)].map((n, i) =>
-      <Star key={i} selected={i < starsSelected} onClick={onRate}/>
+      <Star key={i} selected={i < starsSelected} onClick={() => onRate(i + 1)}/>
     )}
     <p>{starsSelected} of {totalStars} stars</p>
   </div>
